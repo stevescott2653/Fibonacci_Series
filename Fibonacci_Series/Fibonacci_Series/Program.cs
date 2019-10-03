2bf1f9a525dbc1e9c44e084031e58497c4f1539b
Steven Scott <stevescott2653@gmail.com>
2019-10-03 17:24:22 -0400

    Add files via upload

---
+++ Fibonacci_Series/Fibonacci_Series/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    public class FibonacciExample
+    {
+        public static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.Write("Enter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " "); // printing 0 and 1
+            for (int j = 2; j < number; j++) // loop starts from 2 because 0 and 1 are already printed
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+
+        }
+    }
+}
